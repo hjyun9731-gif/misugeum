23ab28b4578c1a2352078f318d1cbea711e6d4df
hjyun9731-gif <hjyun9731@gmail.com>
2026-05-27 19:21:13 +0900

    Fix: 반영대기 탭은 협회비/관리비만 표시, 폐지/양도 제외

--- main.py
+++ main.py
@@ -7153,8 +7153,8 @@ def pending_board_page(
             # 협회가입/택배신규 등은 반영대기 탭에서 협회비/관리비로 표시
             pt = BILLING_TO_PENDING_PT.get(pt_norm, pt_norm)
             st = str(getattr(bp, "reflect_status", "") or "반영대기")
-            # 협회비/관리비/폐지/양도만 반영대기 탭에 표시
-            if pt not in ["협회비", "관리비", "폐지", "양도"]:
+            # 협회비/관리비만 반영대기 탭에 표시, 폐지/양도는 제외
+            if pt not in ["협회비", "관리비"]:
                 continue
             if st in ["처리대기", "부과대수상세"]:
                 st = "반영대기"
